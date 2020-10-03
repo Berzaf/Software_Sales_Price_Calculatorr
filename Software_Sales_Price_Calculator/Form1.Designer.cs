9388a1bc4e2dd1a97fde5b6b3191aa1fac0f2675
Berzaf Berhane <berzafberhane@Berzafs-MacBook-Pro-2.local>
2020-10-02 21:28:16 -0500

    first commit

---
+++ Software_Sales_Price_Calculator/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Software_Sales_Price_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.lblNumPackages = new System.Windows.Forms.Label();
+            this.txtNumOfPackages = new System.Windows.Forms.TextBox();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.lblDiscount = new System.Windows.Forms.Label();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(156, 374);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(129, 64);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate Price";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(400, 374);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(106, 64);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(603, 374);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(121, 64);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // lblNumPackages
+            // 
+            this.lblNumPackages.AutoSize = true;
+            this.lblNumPackages.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblNumPackages.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumPackages.Location = new System.Drawing.Point(180, 82);
+            this.lblNumPackages.Name = "lblNumPackages";
+            this.lblNumPackages.Size = new System.Drawing.Size(315, 20);
+            this.lblNumPackages.TabIndex = 3;
+            this.lblNumPackages.Text = "Enter number of packages purchased:";
+            // 
+            // txtNumOfPackages
+            // 
+            this.txtNumOfPackages.Location = new System.Drawing.Point(558, 82);
+            this.txtNumOfPackages.Name = "txtNumOfPackages";
+            this.txtNumOfPackages.Size = new System.Drawing.Size(100, 20);
+            this.txtNumOfPackages.TabIndex = 4;
+            this.txtNumOfPackages.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(558, 150);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(100, 20);
+            this.txtDiscount.TabIndex = 6;
+            this.txtDiscount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblDiscount
+            // 
+            this.lblDiscount.AutoSize = true;
+            this.lblDiscount.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblDiscount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiscount.Location = new System.Drawing.Point(410, 150);
+            this.lblDiscount.Name = "lblDiscount";
+            this.lblDiscount.Size = new System.Drawing.Size(85, 20);
+            this.lblDiscount.TabIndex = 5;
+            this.lblDiscount.Text = "Discount:";
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(558, 212);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtTotalPrice.TabIndex = 8;
+            this.txtTotalPrice.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.AutoSize = true;
+            this.lblTotalPrice.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalPrice.Location = new System.Drawing.Point(281, 212);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(214, 20);
+            this.lblTotalPrice.TabIndex = 7;
+            this.lblTotalPrice.Text = "Total price after discount:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Software_Sales_Price_Calculator.Properties.Resources.bg_img;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.lblTotalPrice);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.lblDiscount);
+            this.Controls.Add(this.txtNumOfPackages);
+            this.Controls.Add(this.lblNumPackages);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
+            this.Text = "Software Sales Price Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label lblNumPackages;
+        private System.Windows.Forms.TextBox txtNumOfPackages;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.Label lblDiscount;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.Label lblTotalPrice;
+    }
+}
+
---
+++ Software_Sales_Price_Calculator/Form1.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * Author Name: Berzaf Teklu
+ * Date 10/02/2020
+ * 
+ * Create an application that lets the user enter the number of packages purchased. 
+ * The program should then display the amount of the discount and the total amount 
+ * of the purchase after the discount. 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Sales_Price_Calculator
+{
+    public partial class Form1 : Form
+    {
+        // Constant fields
+        const double packagePrice = 99.00;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numOfPackages;
+
+                if(int.TryParse(txtNumOfPackages.Text, out numOfPackages) && numOfPackages >= 0)
+                {
+                    const double discountPercentageTwenty = 0.20;
+                    const double discountPercentageThirty = 0.30;
+                    const double discountPercentageForty = 0.40;
+                    const double discountPercentageFifty = 0.50;
+
+                    if (numOfPackages < 10)
+                    {
+                        double orignalTotalPrice = numOfPackages * packagePrice;
+                        double discount = 0.0;
+                        double totalPrice = orignalTotalPrice - discount;        // Calculate total price after discount.
+
+                        // Display discount and total price.
+
+                        txtDiscount.Text = discount.ToString("c");
+                        txtTotalPrice.Text = totalPrice.ToString("c");
+
+
+                    }
+                    else if (numOfPackages >= 10 && numOfPackages <= 19)
+                    {
+                        double orignalTotalPrice = numOfPackages * packagePrice;
+                        double discount = orignalTotalPrice * discountPercentageTwenty;
+                        double totalPrice = orignalTotalPrice - discount;                  // Calculate total price after discount.
+
+                        // Display discount and total price.
+                        txtDiscount.Text = discount.ToString("c");
+                        txtTotalPrice.Text = totalPrice.ToString("c");
+
+                    }
+                    else if (numOfPackages >= 20 && numOfPackages <= 49)
+                    {
+                        double orignalTotalPrice = numOfPackages * packagePrice;             // Calculate orignal price
+                        double discount = orignalTotalPrice * discountPercentageThirty;
+                        double totalPrice = orignalTotalPrice - discount;                    // Calculate total price after discount.
+
+                        // Display discount and total price.
+                        txtDiscount.Text = discount.ToString("c");
+                        txtTotalPrice.Text = totalPrice.ToString("c");
+                    }
+                    else if (numOfPackages >= 50 && numOfPackages <= 99)
+                    {
+                        double orignalTotalPrice = numOfPackages * packagePrice;
+                        double discount = orignalTotalPrice * discountPercentageForty;
+                        double totalPrice = orignalTotalPrice - discount;                    // Calculate total price after discount.
+
+                        // Display discount and total price.
+                        txtDiscount.Text = discount.ToString("c");
+                        txtTotalPrice.Text = totalPrice.ToString("c");
+                    }
+                    else if (numOfPackages >= 100)
+                    {
+                        double orignalTotalPrice = numOfPackages * packagePrice;
+                        double discount = orignalTotalPrice * discountPercentageFifty;       
+                        double totalPrice = orignalTotalPrice - discount;                    // Calculate total price after discount.
+
+                        // Display discount and total price.
+                        txtDiscount.Text = discount.ToString("c");
+                        txtTotalPrice.Text = totalPrice.ToString("c");
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Display the default error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // clear all the input and output controls.
+            txtNumOfPackages.Text = "";
+            txtDiscount.Text = "";
+            txtTotalPrice.Text = "";
+
+            // Set focus to TextBox.
+            txtNumOfPackages.Focus();
+
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form
+            this.Close();
+        }
+    }
+}
